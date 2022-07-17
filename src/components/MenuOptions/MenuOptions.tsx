edf1fe50571b210be0f36ae1c31195ffcc89ac35
leandrovi <leandrofernandesvieira@gmail.com>
2022-07-17 09:53:25 -0300

    feat: remove watch conference button option

--- src/components/MenuOptions/MenuOptions.tsx
+++ src/components/MenuOptions/MenuOptions.tsx
@@ -21,13 +21,13 @@ export function MenuOptions() {
         </Link>
       </div>
 
-      <div>
+      {/* <div>
         <FaArrowRight size={36} color="#FFFFFF" />
 
         <Link href="https://aovivo.movimentoflores.com.br">
           <a>Acessar conferência</a>
         </Link>
-      </div>
+      </div> */}
     </Container>
   );
 }
