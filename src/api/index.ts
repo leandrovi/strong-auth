2114a5ee3d53bbaa52d6b38dd0b874c68a4470ae
leandrovi <leandrofernandesvieira@gmail.com>
2022-07-29 22:05:46 -0300

    fix: add direct cloudfront url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://auth.conferenciaflores.com.br";
+const baseURL = "https://dfmsuhz45gk0a.cloudfront.net";
 
 export const api = axios.create({
   baseURL,
