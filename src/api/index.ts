28047fbb1ab6328dd00282fd9e02fbc600e67a8b
leandrovi <leandrofernandesvieira@gmail.com>
2022-07-29 22:10:37 -0300

    fix: add new domain to auth endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://auth.conferenciaflores.com.br";
+const baseURL = "https://auth.movimentoflores.com.br";
 
 export const api = axios.create({
   baseURL,
